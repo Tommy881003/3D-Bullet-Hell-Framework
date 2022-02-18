231530a710641d4a85886357402daa9ecde26571
Tommy031088 <38522489+Tommy881003@users.noreply.github.com>
2022-02-18 16:52:15 +0800

    2022.02.18
    feat: 加入追蹤彈（曳光彈）
    
    調整項目：
    1. 調整BHTracerUpdater的實作功能

--- Assets/Scripts/BulletHell/Category_CoreExtension/BHTracerUpdater.cs
+++ Assets/Scripts/BulletHell/Category_CoreExtension/BHTracerUpdater.cs
@@ -16,14 +16,14 @@ namespace BulletHell3D
             public float speed;
             
             // Can this bullet trace the player? (Prevent over-tracing.)
-            // Over-tracing are those bullets which have passes though player, which then turn around and strike back.
+            // Over-tracing happens on bullets which have passes though player, which then turn around and strike back.
             public bool canTrace = true;
             // Is the bullet suffering from "fake" over-tracing?
             public bool fakeOverTrace = true; 
             // Current continuous frames that have over-tracing.
             public int overTraceCounter = 0;
             // Maximum frames allowed for over-tracing.
-            public const int overTraceFrameLimit = 2;
+            public const int overTraceFrameLimit = 3;
 
             // TODO: Seems to miss something...?
         }
@@ -49,6 +49,7 @@ namespace BulletHell3D
         private const float tracerSpeed = 20;
         private const float tracerTrailTime = 0.5f;
         private const float tracerRotateStrenth = 0.05f;
+        private const float overTraceThreshold = 0.75f;
 
         private BHTrailPool trailPool;
         private Player player;
@@ -79,6 +80,19 @@ namespace BulletHell3D
                 addons.Add(pair.Item2);
             }
 
+            /*
+            // Current method of checking over-tracing is by comparing dot value with a threshold.
+            // Which is basically checking whether the bullet is "steering sharply" or not.
+            // This could lead to false-positive. (AKA the algorithm thinks the bullet is over-tracing, but its actually not.)
+            // For example: the bullet is fired in the opposite direction of which it should go.
+            // To cope with this, we need to observe the behaviour of tracing bullets.
+            // There are two possible cases for a traceing bullet:
+            //
+            // 1. Steering lightly → Steering sharply ("actual" over-tracing)
+            // 2. Steering sharply ("fake" over-tracing) → Steering lightly → Steering sharply ("actual" over-tracing)
+            //
+            // We want to prevent "fake" over-tracing, which only happens at the "early" stage of a tracing bullet.
+            */
             for(int i = 0; i < length; i++)
             {
                 bullets[i].SetPosition(bullets[i].position + addons[i].forward * addons[i].speed * deltaTime);
@@ -88,12 +102,12 @@ namespace BulletHell3D
                 
                 Vector3 bulletToPlayer = (player.transform.position - bullets[i].position).normalized;
                 
-                // Shit, this is not working...
                 float dot = Vector3.Dot(addons[i].forward, bulletToPlayer);
-                if(addons[i].fakeOverTrace && dot > 0)
+                
+                if(addons[i].fakeOverTrace && dot >= overTraceThreshold)
                     addons[i].fakeOverTrace = false;
 
-                addons[i].overTraceCounter = (!addons[i].fakeOverTrace && dot < 0)? addons[i].overTraceCounter++ : 0;
+                addons[i].overTraceCounter = (!addons[i].fakeOverTrace && dot < overTraceThreshold)? ++addons[i].overTraceCounter : 0;
                 if(addons[i].overTraceCounter >= TracerAddon.overTraceFrameLimit)
                 {
                     addons[i].canTrace = false;
@@ -173,8 +187,31 @@ namespace BulletHell3D
             }
         }
 
+        /*
         // We use a queue to store the bullet instead of add it into the list immediately to prevent list length mismatch in BHManager.
         // These will be added later into list in the UpdateBullet function.
+        //
+        // "Why does the mismatch happens anyway?" you ask? Well, let's take a look at order of execution in BHManager.
+        // The simplified execution order looks like this:
+        //  
+        // [ 1. Gather raycast result & check bullet collisions. ]
+        //  ↓                                                   ↑
+        // [ 2. Update bullets. ]                              loop
+        //  ↓                                                   |
+        // [ 3. Assign and kick raycast jobs. ] ----------------┘
+        //
+        // [ 4. (By another script) Add bullets into an Updatable IMMEDIATELY ] ← Would cause error in step 1.
+        //
+        // In step 1 the number of raycasts should equals to the number of total bullets. (One bullet, one raycast.)
+        // Step 2 removes invalid bullets, and updates positions of remaining bullets.
+        // Step 3 creates the same amount of raycast commands of the remaining bullets (step 2), then dispatchs the jobs.
+        // On its own, these three steps should not create any error. However, consider step 4 from another script.
+        // Step 4 alter the total amount of bullets AFTER the jobs are dispatched. (More precisely, AFTER step 3, BEFORE step 1 in new loop.)
+        // Now the number of raycast does not equals to the number of total bullets anymore, thus create error in step 1.
+        //
+        // So, after knowing the cause of error, the solution would be simple.
+        // When adding bullets (step 4), we delayed the request till the collision checking process is completed. (AFTER step 1 is done)
+        */
         private void CreateTracerBullet(BHRenderObject renderObject, Vector3 position, Vector3 forward)
         {
             BHBullet newBullet = new BHBullet(position, renderObject);
